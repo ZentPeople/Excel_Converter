2a60088a70b07fc1ada869be10f5a7874bea02a3
Mohan <mohan@zentpeople.com>
2021-01-08 22:58:22 +0530

    Added Environment Variable

--- api/yahooFinance.js
+++ api/yahooFinance.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const yahooFinance = axios.create({
   baseURL: "https://apidojo-yahoo-finance-v1.p.rapidapi.com",
   headers: {
-    "x-rapidapi-key": "901fc31141msh9b1a2a61a735ef3p13909ejsn71de2f72c8eb",
+    "x-rapidapi-key": process.env.RAPID_API_KEY,
     "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
     useQueryString: true,
   },
--- api/yahooFinanceLowLatency.js
+++ api/yahooFinanceLowLatency.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const yahooFinanceLowLatency = axios.create({
   baseURL: "https://yahoo-finance-low-latency.p.rapidapi.com/v2/finance",
   headers: {
-    "x-rapidapi-key": "901fc31141msh9b1a2a61a735ef3p13909ejsn71de2f72c8eb",
+    "x-rapidapi-key": process.env.RAPID_API_KEY,
     "x-rapidapi-host": "yahoo-finance-low-latency.p.rapidapi.com",
     useQueryString: true,
   },
