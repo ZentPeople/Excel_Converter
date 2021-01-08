9b0d441ae19dece3c7a45a276a01045cc384ba99
Mohan <mohan@zentpeople.com>
2021-01-08 21:42:38 +0530

    Setup yahoo finance api with axios

---
+++ api/yahooFinance.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "https://apidojo-yahoo-finance-v1.p.rapidapi.com",
+  headers: {
+    "x-rapidapi-key": "901fc31141msh9b1a2a61a735ef3p13909ejsn71de2f72c8eb",
+    "x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com",
+    useQueryString: true,
+  },
+});
